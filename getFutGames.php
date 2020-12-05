a2f9c6eba1b669fb7024f2cb02a3cacbbf79e1a4
Diana Parreira <dscparreira@gmail.com>
2020-12-05 17:36:15 +0000

    getFutGames.php
    Get data from db

---
+++ getFutGames.php
@@ -0,0 +1,67 @@
+<?php
+
+ $fut= new getFutGames();
+require_once '../League.php';
+require_once '../Game.php';
+
+ class getFutGames{
+    public $servername = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $conn;
+    public $leagues;
+
+
+    public function __construct(){
+        $this->leagues=array();
+        $this->conn = new mysqli($this->servername, $this->username, $this->password);
+        $this->conn->select_db('FlashscoreDB');
+        $this->getGames($this->conn);
+        $this->conn->close();
+    }
+
+    public function getGames($conn){
+
+
+        $sql="SELECT * FROM FootLeagues";
+
+        $leaguesDB= $conn->query($sql);
+
+        if($leaguesDB->num_rows>0){
+         while ($leagueDB=$leaguesDB->fetch_assoc()){
+             $leagueTemp = new League();
+             $leagueTemp->setLeagueName($leagueDB["nome"]);
+             $l=$leagueDB["id"];
+
+             $sql="SELECT * FROM footgames WHERE league_id ='$l' ";
+             $gamesOfLeagues=$conn->query($sql);
+             $gamesTemp=array();
+             while ($gamesOfLeagues=$gamesOfLeagues->fetch_assoc()){
+
+                 $game = new Game($gamesOfLeagues["game_time"],
+                     $gamesOfLeagues["home_team"],
+                     $gamesOfLeagues["away_team"],
+                     $gamesOfLeagues["game_link"],
+                     $gamesOfLeagues["hGoals"],
+                     $gamesOfLeagues["aGoals"],
+                     $gamesOfLeagues["game_status"]
+                 );
+                 $game->setGameInfo($gamesOfLeagues["game_info"]);
+                 array_push($gamesTemp,$game);
+
+             }
+
+             $leagueTemp->pushJogos($gamesTemp);
+
+         }
+
+         array_push($this->leagues,$leagueTemp);
+            echo json_encode($this->leagues);
+        }else{
+            echo"nada";
+        }
+
+
+    }
+
+}
